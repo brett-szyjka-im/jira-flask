48d5046fb1d3f6b89faaa89cdcb74ad3309c21d2
Brett Szyjka <brett.szyjka@extendhealth.com>
2021-02-02 18:40:33 -0500

    autocommit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/infrastructure-request', methods=['POST'])
 def jiraEndpoint():
-	request_data = request.get()
+	request_data = request.get_data(as_text=True)
 	print(infraRequest.parseData(request_data))
 
 @app.route('/api/health', methods=['GET'])
