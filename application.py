f07d697e6cc95f4849f09ab1177061c9b0b26564
Brett Szyjka <brett.szyjka@extendhealth.com>
2021-02-02 14:34:44 -0500

    removed health endpoint

--- application.py
+++ application.py
@@ -1,10 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/health')
-def health():
-	return 'healthy'
-
 @app.route('/jira-endpoint', methods=['POST'])
 def jiraEndpoint(payload):
 	print(payload)
