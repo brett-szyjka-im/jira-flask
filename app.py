f8068c7c2aaed8054218bbd655e393e5e9171fcd
Brett Szyjka <brett.szyjka@extendhealth.com>
2021-02-02 14:55:52 -0500

    autocommit

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/jira-endpoint', methods=['POST'])
 def jiraEndpoint(payload):
 	print(payload)
+
+@app.route('/health', methods=['GET'])
+def health():
+	return "healthy"
---
+++ tests/test_flask.py
@@ -0,0 +1,24 @@
+import os
+import tempfile
+import requests
+
+import pytest
+
+from flaskr import flaskr
+
+
+@pytest.fixture
+def client():
+	db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+	flaskr.app.config['TESTING'] = True
+
+	with flaskr.app.test_client() as client:
+		with flaskr.app.app_context():
+			flaskr.init_db()
+		yield client
+
+	os.close(db_fd)
+	os.unlink(flaskr.app.config['DATABASE'])
+
+def test_site_up(client):
+	
